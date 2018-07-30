19c4d3ca6eaf9e1c731e3e93ca3052ae633be868
Jobin B.A <38902140+WhyBranium@users.noreply.github.com>
2018-07-30 02:12:02 -0700

    kronic miracle

--- aosip.mk
+++ aosip.mk
@@ -18,14 +18,14 @@
 
 $(call inherit-product, device/xiaomi/kenzo/full_kenzo.mk)
 
-# Inherit some common Cardinal-AOSP stuff.
-$(call inherit-product, vendor/cardinal/common.mk)
+# Inherit some common AOSiP stuff.
+$(call inherit-product, vendor/aosip/config/common.mk)
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := cardinal_kenzo
+PRODUCT_NAME := aosip_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 3
 TARGET_VENDOR := Xiaomi
